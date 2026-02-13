5ab44859c99a1861d7c22855aab32fdbe21cb992
KevinRafa30 <kevinrafael3023@gmail.com>
2026-02-13 02:48:56 -0400

    Agregando ruta de lista

--- sistema_laboratorios/urls.py
+++ sistema_laboratorios/urls.py
@@ -4,5 +4,7 @@ from gestion import views  # <--- Importante: Importar tus vistas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reservar/', views.crear_reservacion, name='crear_reservacion'), # <--- Nueva ruta
+    path('reservar/', views.crear_reservacion, name='crear_reservacion'), 
+    path('lista/', views.lista_reservaciones, name='lista_reservaciones'), # <--- ESTA NUEVA
+
 ]
